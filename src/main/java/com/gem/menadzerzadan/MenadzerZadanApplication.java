73c32fa5ff61036b0c4cdd7d07a1c5ca5bfee097
morfidon <armiksos@gmail.com>
2025-02-25 13:32:44 +0100

    first init

---
+++ src/main/java/com/gem/menadzerzadan/MenadzerZadanApplication.java
@@ -0,0 +1,13 @@
+package com.gem.menadzerzadan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenadzerZadanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenadzerZadanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gem/menadzerzadan/MenadzerZadanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gem.menadzerzadan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenadzerZadanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
