d205bcc5d394895ad3082b062046b6a702478c1c
morfidon <armiksos@gmail.com>
2025-03-06 12:57:23 +0100

    pierwszy kontroler

--- src/main/java/com/gem/menadzerzadan/MenadzerZadanApplication.java
+++ src/main/java/com/gem/menadzerzadan/MenadzerZadanApplication.java
@@ -2,19 +2,15 @@ package com.gem.menadzerzadan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
+
 public class MenadzerZadanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MenadzerZadanApplication.class, args);
     }
-    @GetMapping("/home")
-    public String home() {
-        return "Hello World!";
-    }
+
 
 }
