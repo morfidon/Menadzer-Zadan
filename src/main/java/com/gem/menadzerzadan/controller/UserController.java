7c08165b7d506836d64da22191199f3444a02e07
morfidon <armiksos@gmail.com>
2025-03-10 11:51:16 +0100

    Refactor /users endpoint to return JSON user data
    Updated the /users endpoint to return a list of User objects in JSON format instead of a plain string. This change improves the API response structure, aligning it with standard REST practices.

--- src/main/java/com/gem/menadzerzadan/controller/UserController.java
+++ src/main/java/com/gem/menadzerzadan/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.gem.menadzerzadan.controller;
 
+import com.gem.menadzerzadan.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
     @GetMapping("/users")
-    public String getUsers() {
-        return "Lista ";
+    public List<User> getUsers()
+    {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User(1, "admin", "admin", "<EMAIL>"));
+        users.add(new User(2, "user", "user", "<EMAIL>"));
+        users.add(new User(3, "user2", "user2", "<EMAIL>"));
+
+        return users;
     }
 }
-//MODEL
+//JSON
