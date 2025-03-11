32d9c91d1f03e1e8b1c61184c6d072fd2b808fd9
morfidon <armiksos@gmail.com>
2025-03-11 13:24:11 +0100

    Refactor UserController to simplify endpoint mappings.
    Removed redundant endpoint paths by adding a base @RequestMapping annotation. This improves consistency and reduces duplication in URL definitions for the controller methods.

--- src/main/java/com/gem/menadzerzadan/controller/UserController.java
+++ src/main/java/com/gem/menadzerzadan/controller/UserController.java
@@ -3,15 +3,13 @@ package com.gem.menadzerzadan.controller;
 import com.gem.menadzerzadan.model.User;
 import com.gem.menadzerzadan.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -21,12 +19,12 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers()
     {
         return userRepository.findAll();
     }
-    @PostMapping("/users")
+    @PostMapping
     public User addUser(@RequestBody User user)
     {
         return userRepository.save(user);
