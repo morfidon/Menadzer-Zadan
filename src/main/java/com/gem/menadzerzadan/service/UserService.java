0aa3949ac9179e6922345ec4d4086d27e79929cc
morfidon <armiksos@gmail.com>
2025-03-17 12:22:55 +0100

    Add password encoding to UserService
    Integrated BCryptPasswordEncoder to encode user passwords before saving them to the database. This enhances security by ensuring passwords are stored in a hashed format.

--- src/main/java/com/gem/menadzerzadan/service/UserService.java
+++ src/main/java/com/gem/menadzerzadan/service/UserService.java
@@ -5,6 +5,9 @@ import com.gem.menadzerzadan.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,10 +18,11 @@ import java.util.Optional;
 public class UserService implements UserDetailsService
 {
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
     public UserService(UserRepository userRepository)
     {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public List<User> getUsers()
@@ -28,10 +32,10 @@ public class UserService implements UserDetailsService
 
     public User addUser(@RequestBody User user)
     {
-
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
-
+//pizza123 - Vnmacjvb2n3r2!#RFVADSv235t12#% - PasswordEncoder
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
