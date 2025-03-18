8dc17af2761ff9c5255e15549da195eb386d0aad
morfidon <armiksos@gmail.com>
2025-03-18 14:14:51 +0100

    Restrict user endpoint access to ADMIN role
    Updated security configuration to require ADMIN role for accessing "/users/**" endpoints. This enhances security by limiting access and ensures only authorized administrators can interact with these routes. Other endpoints remain publicly accessible.

--- src/main/java/com/gem/menadzerzadan/config/SecurityConfig.java
+++ src/main/java/com/gem/menadzerzadan/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig
     {
         http.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
-                        .requestMatchers("/users/**").authenticated()
+                        .requestMatchers("/users/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
         ).formLogin(formLogin -> formLogin.loginPage("/login").permitAll());
 
